99696b4411c3498723115c0323de6cc600d352fa
serenzun <szunino@inogs.com>
2020-05-02 17:42:10 +0200

    example:delete a goal

--- prep/AO/access_prep.r
+++ prep/AO/access_prep.r
@@ -1,6 +1,10 @@
 ## access_prep.r
 
-## In this example we will prepare local data that will substitute Global OHI data for the data layer `ao_access`. The 'local data' will be dummy data in this example. We will import data that is already formatted as a data layer for the Toolbox, save it in the "layers" folder, and finally make sure it is properly registered in `layers.csv` and `scenario_data_years.csv`.
+## In this example we will prepare local data that will substitute Global OHI data for 
+#the data layer `ao_access`. The 'local data' will be dummy data in this example. 
+#We will import data that is already formatted as a data layer for the Toolbox, 
+#save it in the "layers" folder, and finally make sure it is properly registered 
+#in `layers.csv` and `scenario_data_years.csv`.
 
 ## ***************************************************************************** ##
 
@@ -18,7 +22,9 @@ d <- readr::read_csv(data_file)
 summary(d)
 
 
-## make dummy changes to data by adding random numbers within a small range. This will keep the values between 0-1, which is probably what the models are expecting, and we won't be changing them at this point.
+## make dummy changes to data by adding random numbers within a small range. 
+#This will keep the values between 0-1, which is probably what the models are expecting, 
+#and we won't be changing them at this point.
 d2 <- d %>%
   mutate(value = runif(value, min=0, max=0.2))
 
@@ -28,4 +34,5 @@ d2 <- d %>%
 readr::write_csv(d2, file.path(dir_layers, "ao_access_demo2017.csv"))
 
 
-## You will see a notification in the "Git" window in RStudio once the layer is saved successfully. Now we need to register this layer in "layers.csv". Let's go back to the tutorial and see how to do that.
+## You will see a notification in the "Git" window in RStudio once the layer is saved successfully.
+#Now we need to register this layer in "layers.csv". Let's go back to the tutorial and see how to do that.
